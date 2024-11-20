14e7735efdfdf5649b934e5d3eedf91deef558e1
Adenilson <adekonzelmann@gmail.com>
2024-11-20 06:01:52 -0300

    feat: Adiciona lógica inicial do frontend

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Importa os produtos e a função de renderização
+import { products, renderProducts } from './modules/productList.js';
+
+// Chama a função para renderizar os produtos na lista
+renderProducts(products, "product-list");
